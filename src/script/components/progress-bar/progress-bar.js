da79e800d50ef7f3545e5e69b94fbb8eb1bd4498
painofhail <painofhail@yandex.ru>
2021-07-22 17:15:22 +0300

    closure (замыкание)

--- src/script/components/progress-bar/progress-bar.js
+++ src/script/components/progress-bar/progress-bar.js
@@ -1,13 +1,15 @@
-const progressBar = document.querySelector('.progress-bar .progress-bar__current');
+(function (){
+	const progressBar = document.querySelector('.progress-bar .progress-bar__current');
 
-fillProgressLine();
+	fillProgressLine();
 
-window.addEventListener('scroll', fillProgressLine);
-window.addEventListener('resize', fillProgressLine);
+	window.addEventListener('scroll', fillProgressLine);
+	window.addEventListener('resize', fillProgressLine);
 
-function fillProgressLine () {
-	const fullHeight = document.body.scrollHeight;
-	const innerHeight = window.innerHeight;
-	const pageYOffset = window.pageYOffset;
-	progressBar.style.width = (pageYOffset * 100) / (fullHeight - innerHeight) + '%';
-}
+	function fillProgressLine () {
+		const fullHeight = document.body.scrollHeight;
+		const innerHeight = window.innerHeight;
+		const pageYOffset = window.pageYOffset;
+		progressBar.style.width = (pageYOffset * 100) / (fullHeight - innerHeight) + '%';
+	}
+})();
--- src/script/components/switch-theme/switch-theme.js
+++ src/script/components/switch-theme/switch-theme.js
@@ -1,29 +1,32 @@
-const switchTheme = document.querySelector('.switch-theme');
-const body = document.querySelector('body');
+(function (){
+	const switchTheme = document.querySelector('.switch-theme');
+	const body = document.querySelector('body');
 
-changeLocalStorageTheme();
+	changeLocalStorageTheme();
 
-switchTheme.addEventListener('click', toggleTheme)
+	switchTheme.addEventListener('click', toggleTheme)
 
-function changeLocalStorageTheme () {
-	// Initial theme setting
-	if (!localStorage.getItem('theme')) {
-		localStorage.setItem('theme', 'light')
+	function changeLocalStorageTheme () {
+		// Initial theme setting
+		if (!localStorage.getItem('theme')) {
+			localStorage.setItem('theme', 'light')
+		}
+
+		// Check, if dark theme
+		if (localStorage.getItem('theme') === 'dark') {
+			toggleTheme();
+		}
 	}
 
-	// Check, if dark theme
-	if (localStorage.getItem('theme') === 'dark') {
-		toggleTheme();
-	}
-}
+	function toggleTheme () {
+		switchTheme.classList.toggle('dark-theme');
+		body.classList.toggle('dark-theme');
 
-function toggleTheme () {
-	switchTheme.classList.toggle('dark-theme');
-	body.classList.toggle('dark-theme');
-
-	if (body.classList.contains('dark-theme')) {
-		localStorage.setItem('theme', 'dark')
-	} else {
-		localStorage.setItem('theme', 'light')
+		if (body.classList.contains('dark-theme')) {
+			localStorage.setItem('theme', 'dark')
+		} else {
+			localStorage.setItem('theme', 'light')
+		}
 	}
-}
+
+})();
